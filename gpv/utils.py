5fbcc5bf5332b9d514dcefb185a851b8752393c8
henry-yeh <haoran-ye@outlook.com>
2024-09-04 23:25:31 +0800

    add query gen

--- gpv/utils.py
+++ gpv/utils.py
@@ -1,10 +1,47 @@
 import json
+import os
 
+import torch
 from transformers import pipeline
 try:
     from .models import LLMModel
 except ImportError:
     from models import LLMModel
+
+
+
+def get_valence_value(valence_vec):
+    """
+    Returns the valence of the value vector
+    """
+    if valence_vec[0] > valence_vec[1] and valence_vec[0] > valence_vec[2]:
+        return valence_vec[0].item()
+    elif valence_vec[1] > valence_vec[0] and valence_vec[1] > valence_vec[2]:
+        return - valence_vec[1].item()
+    else:
+        return None
+
+
+def get_valence_label(valence_vec):
+    """
+    Returns the valence of the value vector
+    """
+    if valence_vec[0] > valence_vec[1] and valence_vec[0] > valence_vec[2]:
+        return "Supports"
+    elif valence_vec[1] > valence_vec[0] and valence_vec[1] > valence_vec[2]:
+        return "Opposes"
+    else:
+        return "Either"
+
+
+def get_score(valence_vec):
+    """
+    Returns the score of the value
+    """
+    if valence_vec[2] > valence_vec[0] and valence_vec[2] > valence_vec[1]:
+        return None
+    return valence_vec[0] - valence_vec[1]
+
 
 def coref_resolve_simple(entities: list[str]) -> list[str]:
     """
@@ -84,24 +121,68 @@ def coref_resolve_llm(entities: list[list[str]], model_name="Qwen1.5-110B-Chat")
 
     return entities, entity2coref
 
-def gen_queries_for_perception_retrieval(value: str, model_name: str="Qwen1.5-110B-Chat"):
+def gen_queries_for_perception_retrieval(value: str, measurement_subject: str, model_name: str="Qwen1.5-110B-Chat"):
     """
-    Generate queries for perception retrieval.
+    Generate queries via LLM for perception retrieval.
     """
-    system_prompt = """You are an expert in Psychology and Human Values. You are provided with a value and you need to write two items that support and oppose the value, respectively. You respond using JSON format. An example is provided below:
+
+    system_prompt = """[Background] You are an expert in Psychology and Human Values. Given a specific value and a person, your task is to write five diverse items that support it and five diverse items that oppose it. The items should be in the same language as the person's name. You respond using JSON format. Examples are provided below:
 ---
-**Value:** Self-Direction
-**Your Response:** {"support": "Thinking up new ideas and being creative is important to me.", "oppose": "I think it is important to do what I'm told."}
+[Example 1]
+**User Input:** Value: Self-Direction; Person: Henry
+**Your Response:** {
+    "support": [
+        "Thinking up new ideas and being creative is important to Henry.",
+        "Henry values making his own decisions about what he does in life.",
+        ...
+    ],
+    "oppose": [
+        "Henry thinks it is important to do what he's told.",
+        "Henry prefers to follow the guidance of others rather than trust his own judgment.",
+        ...
+    ]
+}
 ---
-Strictly follow the format of the example and do not add any additional information.
+[Example 2]
+**User Input:** Value: Hedonism; Person: 小明
+**Your Response:** {
+    "support": [
+        "小明认为人们应该追求快乐。",
+        "小明喜欢参加各种能够带来乐趣的活动。",
+        ...
+    ],
+    "oppose": [
+        "小明觉得牺牲眼前的快乐以换取长远的利益是值得的。",
+        "小明认为自律和克制比及时行乐更重要。",
+        ...
+    ]
+}
+---
 """
-    user_prompt = "**Value:** " + value
+    user_prompt = f"Value: {value}; Person: {measurement_subject}"
     model = LLMModel(model_name, system_prompt=system_prompt)
     result = model([user_prompt], batch_size=1, response_format="json")[0]
     result_json = json.loads(result.strip("```json").strip("```"))
     support, oppose = result_json["support"], result_json["oppose"]
     return support, oppose
     
+def get_openai_sentence_embedding(input_texts: list[str], model_name: str='text-embedding-3-large') -> list[torch.Tensor]:
+    """
+    Get the sentence embeddings of the input texts using OpenAI API.
+
+    Args:
+        input_texts (list[str]): A list of input texts.
+        model_name (str): The name of the model.
+        api_key (str): The API key of OpenAI.
+    """
+    from openai import OpenAI
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    response = client.embeddings.create(
+        input=input_texts,
+        model=model_name,
+    )
+    embeddings = [response.data[i].embedding for i in range(len(response.data))]
+    return embeddings
 
 
 if __name__ == "__main__":
